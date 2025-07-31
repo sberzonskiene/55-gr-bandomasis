ea7c49350766fcb1c96d80935a8b73c6a7ba2ee5
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-31 13:09:41 +0300

    starts

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { HomePage } from './pages/public/Home';
+import { PublicLayout } from './templates/PublicLayout';
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PublicLayout />}>
+          <Route path='/' index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+
---
+++ client/src/pages/public/Home.jsx
@@ -0,0 +1,6 @@
+export function HomePage() {
+    return (
+        <h1>HOME PAGE CONTENT</h1>
+    )
+
+}
