63cb88bdac22495f9712e9e10af183fe7c260a52
sberzonskiene <s.berzonskiene@gmail.com>
2025-08-04 10:07:05 +0300

    header-footer

--- client/src/templates/PublicLayout.jsx
+++ client/src/templates/PublicLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 export function PublicLayout() {
     return (
         <>
-            <header>HEADER CONTENT</header>
+            <Header />
             <Outlet />
-            <footer>FOOTER CONTENT</footer>
+            <Footer />
         </>
     )
 }
