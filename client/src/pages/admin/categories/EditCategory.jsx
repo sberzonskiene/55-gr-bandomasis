43800fe305b4ddb5e22d4e70461791a3c65b6fd2
sberzonskiene <s.berzonskiene@gmail.com>
2025-08-06 10:01:24 +0300

    category

--- client/src/pages/admin/categories/EditCategory.jsx
+++ client/src/pages/admin/categories/EditCategory.jsx
@@ -1,14 +1,25 @@
 import { AdminPageTitle } from "../../../components/AdminPageTitle";
+import { Alert } from "../../../components/Alert";
 import { AdminCategoryForm } from "../../../components/forms/AdminCategoryForm";
 
 export function AdminEditCategoryPage() {
+    const category = {
+        title: 'Action',
+        url: 'action',
+        description: 'Very action, much movie',
+        status: 'published',
+    };
+
     return (
         <main>
             <AdminPageTitle title="Edit category" />
 
             <div className="container">
                 <div className="row">
-                    <AdminCategoryForm />
+                    <div class="col-12 col-md-9 mt-5">
+                        <Alert text='Norima kategorija nerasta, todel redagavimas yra neimanomas.' />
+                    </div>
+                    <AdminCategoryForm category={category} />
                 </div>
             </div>
         </main>
