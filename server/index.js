398d9c03cd8b0b968d1d832d692dbcb7c3539af1
sberzonskiene <s.berzonskiene@gmail.com>
2025-08-18 11:45:47 +0300

    update

--- server/index.js
+++ server/index.js
@@ -13,6 +13,12 @@ import { getPublicCategories } from './src/api/public/getCategories.js';
 import { getAdminCategories } from './src/api/admin/categories/getCategories.js';
 import { isPublic } from './src/middleware/isPublic.js';
 import { putAdminCategories } from './src/api/admin/categories/putCategories.js';
+import { deleteAdminCategories } from './src/api/admin/categories/deleteCategories.js';
+import { getPublicMovies } from './src/api/public/getMovies.js';
+import { getAdminMovies } from './src/api/admin/movies/getMovies.js';
+import { postAdminMovies } from './src/api/admin/movies/postMovies.js';
+import { putAdminMovies } from './src/api/admin/movies/putMovies.js';
+import { deleteAdminMovies } from './src/api/admin/movies/deleteMovies.js';
 
 const app = express();
 
@@ -36,11 +42,19 @@ app.post('/api/register', isPublic, postPublicRegister);
 app.post('/api/login', isPublic, postPublicLogin);
 
 app.get('/api/categories', getPublicCategories);
+app.get('/api/movies', getPublicMovies);
 
 app.get('/api/login', isAdmin, getLogin);
+
 app.get('/api/admin/categories', isAdmin, getAdminCategories);
 app.post('/api/admin/categories', isAdmin, postAdminCategories);
 app.put('/api/admin/categories/:original_url', isAdmin, putAdminCategories);
+app.delete('/api/admin/categories/:url', isAdmin, deleteAdminCategories);
+
+app.get('/api/admin/movies', isAdmin, getAdminMovies);
+app.post('/api/admin/movies', isAdmin, postAdminMovies);
+app.put('/api/admin/movies/:original_url', isAdmin, putAdminMovies);
+app.delete('/api/admin/movies/:url', isAdmin, deleteAdminMovies);
 
 app.use((err, req, res, next) => {
     console.log(err);
