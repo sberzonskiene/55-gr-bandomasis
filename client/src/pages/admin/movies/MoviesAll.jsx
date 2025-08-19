bf969cc8b16c1e43be43976ee82d28f8386bd0e8
sberzonskiene <s.berzonskiene@gmail.com>
2025-08-19 14:08:07 +0300

    adm mv table

--- client/src/pages/admin/movies/MoviesAll.jsx
+++ client/src/pages/admin/movies/MoviesAll.jsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { AdminMoviesTable } from "../../../components/admin-movies-table/AdminMoviesTable";
 import { AdminPageTitle } from "../../../components/AdminPageTitle";
 import { MoviesContext } from "../../../context/movies/MoviesContext";
